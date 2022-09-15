9414f47ec68d3e734f53d29539612d38807a530e
menta <jimementa@gmail.com>
2022-09-16 00:28:19 +0200

    Forms reactives update

--- src/app/reactive/basics/basics.component.ts
+++ src/app/reactive/basics/basics.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-basics',
@@ -8,9 +9,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BasicsComponent implements OnInit {
 
-  constructor() { }
+  // miFormulario : FormGroup = new FormGroup ({
+  //   nombre : new FormControl('cualquier nombre'),
+  //   precio : new FormControl(1000),
+  //   existencias : new FormControl(5),
+  // })
+
+  miFormulario : FormGroup = this.fb.group({
+    name :[,   [Validators.required, Validators.minLength(3)]],
+    price :[,  [Validators.required, Validators.min(0)]],
+    stock :[, [Validators.required, Validators.min(0)]]
+  })
+  constructor( private fb: FormBuilder) { }
 
   ngOnInit(): void {
+
+//     this.miFormulario.reset({
+// //puedo asignarle valores para empezar
+//     })
+  }
+
+  invalidField( field : string){
+    return this.miFormulario.controls[field].errors 
+    && this.miFormulario.controls[field].touched
+  }
+
+  save(){
+    if( this.miFormulario.invalid){
+      this.miFormulario.markAllAsTouched()
+      return
+    }
+    this.miFormulario.reset()
   }
 
 }
--- src/app/reactive/dinamics/dinamics.component.ts
+++ src/app/reactive/dinamics/dinamics.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-dinamics',
@@ -8,9 +9,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DinamicsComponent implements OnInit {
 
-  constructor() { }
+  miFormulario : FormGroup = this.fb.group({
+    name :['',[Validators.required, Validators.minLength(3)]],
+    favorites : this.fb.array([
+      ['TEG'],['Ajedrez'] // arreglo de formscontrols
+    ], Validators.required)
+  })
+
+  newFavorite : FormControl = this.fb.control('',Validators.required)
+
+  get favoritesArr(){
+    return this.miFormulario.get('favorites') as FormArray;
+  }
+  constructor( private fb: FormBuilder) { }
 
   ngOnInit(): void {
+  }
+
+
+
+  invalidField( field : string){
+    return this.miFormulario.controls[field].errors 
+    && this.miFormulario.controls[field].touched
+  }
+
+  addFavorite(){
+    if(this.miFormulario.invalid){return}
+
+    this.favoritesArr.push( new FormControl(this.newFavorite.value, Validators.required)); //this.fb.control es igual que new FormControl
+
+    this.newFavorite.reset()
+  }
+  save(){
+    if( this.miFormulario.invalid){
+      this.miFormulario.markAllAsTouched()
+      return
+    }
+  }
+  
+  delete(i : number){
+    this.favoritesArr.removeAt(i)
   }
 
 }
